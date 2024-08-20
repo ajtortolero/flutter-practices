1fddfaf7a1817874fb8e89a8d188c2dddbb8bfe6
Alejandro Tortolero <atortolero@arkkosoft.net>
2024-08-20 15:49:29 -0500

    Agregando Practica #5

---
+++ Practica 4/testing_app/android/app/src/main/kotlin/com/ajtortolero/testing_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ajtortolero.testing_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Practica 5/ecommerce_app/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:ecommerce_app/core/store.dart';
+import 'package:ecommerce_app/pages/cart_page.dart';
+import 'package:ecommerce_app/pages/home_page.dart';
+import 'package:ecommerce_app/utils/router.dart';
+import 'package:ecommerce_app/widgets/themes.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+void main() {
+  runApp(
+    VxState(
+      store: MyStore(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      builder: (context, _) {
+        final themeProvider = Provider.of<ThemeProvider>(context);
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          themeMode: themeProvider.themeMode,
+          theme: Mytheme.lightTheme(context),
+          darkTheme: Mytheme.darkTheme(context),
+          initialRoute: "/",
+          routes: {
+            "/": (context) => const HomePage(),
+            MyRoutes.homeRoute: (context) => const HomePage(),
+            MyRoutes.cartRoute: (context) => const CartPage(),
+          },
+        );
+      },
+    );
+  }
+}
